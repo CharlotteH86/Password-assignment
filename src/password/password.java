8b25220a6aca8edc08e16c684dff7ff062adfd2d
Charlotte <70633528+CharlotteH86@users.noreply.github.com>
2020-12-03 17:14:14 +0100

    Initial commit

---
+++ src/password/password.java
@@ -0,0 +1,32 @@
+package password;
+
+import java.util.Scanner;
+
+public class password {
+
+	public static void main(String[] args) {
+		Scanner reader = new Scanner(System.in);
+
+		String rightPassword = "miyagi123";
+
+		System.out.println("Greeting! Write the right password below to log in: ");
+
+		String passwordInput;
+
+		while(true) {
+			passwordInput=reader.nextLine();
+		if (passwordInput.equals(rightPassword)) {
+			System.out.println("Right Password!");
+			break;
+			
+		} else if(rightPassword != passwordInput){
+			System.out.println("Wrong password, try again: ");
+		}
+	
+		}
+		
+		
+		
+	}
+
+}
